642d0041fd1f6842f10efda3c312f874074b5f95
pratham <prathamvidyarthi@gmail.com>
2026-03-11 09:30:05 +0530

    Delete stepuc2.java

--- stepuc2.java
+++
@@ -1,9 +0,0 @@
-public class OopsDisplay {
-    public static void main(String[] args) {
-        System.out.println("  ___   ___  ____  ____ "); 
-        System.out.println(" / _ \\ / _ \\|  _ \\/ ___|");
-        System.out.println("| | | | | | | |_) \\___ \\ ");
-        System.out.println("| |_| | |_| |  __/ ___) |");
-        System.out.println(" \\___/ \\___/|_|   |____/ ");
-    }
-}
